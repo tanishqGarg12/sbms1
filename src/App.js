01985c3a3af0416a429229e850b88be0e715aa5c
Tanishq Garg <tanishq.garg@nielsen.com>
2026-03-20 18:02:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 import ParticlesBackground from './components/ParticlesBackground';
 import Navbar from './Homepage/Navbar';
 import HeroSection from './Homepage/HeroSection';
@@ -87,6 +88,7 @@ export default function App() {
           </div>
         </div>
       {/* </Router> */}
+      <Analytics />
     </DarkModeProvider>
   );
 }
